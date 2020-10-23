02af9e0d6451139c5f5bd954513ea598518c1677
clancybe <clancybe@bc.edu>
2020-10-23 15:55:42 -0400

    Added stopword removal to preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -4,7 +4,10 @@ import sys
 from string import punctuation
 from string import digits
 
-fname = 'p4kreviews'
+import nltk
+from nltk.corpus import stopwords
+
+fname = 'testReviews'
 
 df = pd.read_csv("datasets/" + fname +".csv")
 
@@ -14,10 +17,34 @@ df['review'] = df['review'].str.replace('[{}]'.format(digits), '')
 df['review'] = df['review'].str.lower()
 
 
-if 'p4k' in fname or 'pitchfork' in fname:
+if 'p4k' in fname or 'pitchfork' in fname or 'test' in fname:
     for index, row in df.iterrows():
         if row['best'] == 1:
             df.it[index, 'review'] = ' '.join(row['review'].split(' ')[3:])  
             
-            
+        #remove all stopwords except 'not'
+        nonStopwords = []
+        sw = stopwords.word('english')
+        for word in row['review'].split(' '):
+            if (not word in sw) or (word == 'not'):
+                nonStopwords.append(word)
+        
+        #concatonate 'not's
+        finalWords = ''
+        x = 0
+        while x < len(nonStopwords):
+            if x < (len(nonStopwords) - 1):
+                if nonStopwords[x] == 'not':
+                    finalWords += ('not ' + nonStopwords[x+1])
+                    x += 1
+                else:
+                    finalWords += (nonStopwords[x] + ' ')
+            else:
+                finalWords += (nonStopwords[x])
+
+        df.it[index, 'review'] = finalWords
+
+#Save to a new csv
+newfname = fname + "Preprocessed"
+df.to_csv("datasets/" + newfname +".csv")
 
