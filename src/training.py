d7b79c391460c52411a01652e7ac612e49b9c9ac
Gavin Bloom <bloomga@bc.edu>
2020-10-29 15:58:55 -0400

    testing added

--- src/training.py
+++ src/training.py
@@ -28,7 +28,7 @@ with open("obj/encoded" + fname + 'Preprocessed.json', "r") as fp:
 with open("obj/" + fname + 'PreprocessedDict.json', "r") as fp:
     review_dict = json.load(fp)
 
-#initialize model
+#set hyperparameters
 #using basic hyperparameters for now, these will be optimized later
 vocab_size = len(review_dict)+1 # +1 accounts for the 0 padding "word"
 output_size = 1
@@ -36,43 +36,49 @@ input_size = 400
 hidden_size = 256
 num_rec_layers = 2
 dropout = 0.5
-net = MusicLSTM(vocab_size, output_size, input_size, hidden_size, num_rec_layers, dropout)
-print(net)
 
 #loss function RMSE
 criterion = RMSELoss
-    
-#optmizer
-optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
 
 # check if CUDA is available
 train_on_gpu = torch.cuda.is_available()
 device = torch.device("cuda" if train_on_gpu else "cpu")
 
-#put into training mode
-net.train()
-
 #split datasets into train and test
-train_x = np.array(reviews[:int(0.8*len(reviews))])
-train_y = np.array(scores[:int(0.8*len(reviews))])
-reserved_test_x = np.array(reviews[int(0.8*len(reviews)):])
-reserved_test_y = np.array(scores[int(0.8*len(reviews)):])             
+train_x = np.array(reviews[:int(0.9*len(reviews))])
+train_y = np.array(scores[:int(0.9*len(reviews))])
+reserved_test_x = np.array(reviews[int(0.9*len(reviews)):])
+reserved_test_y = np.array(scores[int(0.9*len(reviews)):])             
 
-#create k-folds=10 and loop
-kfold = KFold(n_splits=4) #we can lower the splits number for testing
-for fold, (train_index, test_index) in enumerate(kfold.split(train_x, train_y)):
+#create k-folds and loop
+k = 4 #want k=10. we can change k for testing
+kfold = KFold(n_splits=k) 
+model_list = list()
+val_loss_list = list()
+
+for fold, (train_index, val_index) in enumerate(kfold.split(train_x, train_y)):
+    #initialize model
+    net = MusicLSTM(vocab_size, output_size, input_size, hidden_size, num_rec_layers, dropout)
+    print(net)
+
+    #optmizer
+    optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
+
+    #put into training mode
+    net.train() 
+    
     #divide up folds
     train_fold_x = train_x[train_index]
-    test_fold_x = train_x[test_index]
+    val_fold_x = train_x[val_index]
     train_fold_y = train_y[train_index]
-    test_fold_y = train_y[test_index]
+    val_fold_y = train_y[val_index]
 
     #create tensors and dataloaders
     batch_size=50
     train = TensorDataset(torch.FloatTensor(train_fold_x), torch.FloatTensor(train_fold_y))
-    test = TensorDataset(torch.FloatTensor(test_fold_x), torch.FloatTensor(test_fold_y))
+    validate = TensorDataset(torch.FloatTensor(val_fold_x), torch.FloatTensor(val_fold_y))
     train_loader = DataLoader(train, batch_size = batch_size, shuffle = True)
-    test_loader = DataLoader(test, batch_size = batch_size, shuffle = True)
+    val_loader = DataLoader(validate, batch_size = batch_size, shuffle = True)
 
 
     epochs = 4 #we will adjust this after training, see how many epochs before loss stops decreasing
@@ -112,33 +118,81 @@ for fold, (train_index, test_index) in enumerate(kfold.split(train_x, train_y)):
             #calculate loss stats
             #WE WILL INCLUDE MORE DATA GATHERING IN THIS STEP TOO
             if step_counter % 50 == 0: #change the print rate for testing
-                #calculate validation loss
-                #first init a new zeroed hidden state
-                val_hidden = net.init_hidden_state(batch_size, train_on_gpu)
-                val_losses = list()
-
-                net.eval() #put net in eval mode so it doesnt learn from the validation data
-                for inputs, targets in test_loader:
-
-                    inputs = inputs.to(device).long()
-                    targets = targets.to(device).long()
-
-                    #create new hidden state variables
-                    val_hidden = tuple([h.data for h in val_hidden])
-
-                    #get output and then calculate loss
-                    output, val_hidden = net(inputs, val_hidden)
-                    val_loss = criterion(output, targets)
-
-                    val_losses.append(val_loss.item())
-
-                val_loss = np.mean(val_losses)
-                net.train() #set back to training mode
-
                 #CHANGE FROM PRINTING TO STORAGE FOR LATER PLOTTING
                 #CALL PLOTTING FROM SEPERATE IMPORTED FILE, DONT CROWD THIS ONE
-                print("Epoch: {}/{}...".format(e+1, epochs),
-                  "Step: {}...".format(counter),
-                  "Loss: {:.6f}...".format(loss.item()),
-                  "Val Loss: {:.6f}".format(val_loss))
+                print("Fold: {}/{}...".format(fold, k), 
+                      "Epoch: {}/{}...".format(e+1, epochs),
+                      "Step: {}...".format(counter),
+                      "Loss: {:.6f}...".format(loss.item()))
+                  
+        #calculate validation loss
+        #first init a new zeroed hidden state
+        val_hidden = net.init_hidden_state(batch_size, train_on_gpu)
+        val_losses = list()
 
+        net.eval() #put net in eval mode so it doesnt learn from the validation data
+        for inputs, targets in val_loader:
+
+            inputs = inputs.to(device).long()
+            targets = targets.to(device).long()
+
+            #create new hidden state variables
+            val_hidden = tuple([h.data for h in val_hidden])
+
+            #get output and then calculate loss
+            output, val_hidden = net(inputs, val_hidden)
+            val_loss = criterion(output, targets)
+
+            val_losses.append(val_loss.item())
+
+        val_loss = np.mean(val_losses)
+        net.train() #set back to training mode
+
+        #CHANGE FROM PRINTING TO STORAGE FOR LATER PLOTTING
+        #CALL PLOTTING FROM SEPERATE IMPORTED FILE, DONT CROWD THIS ONE
+        print("Val Loss: {:.6f}".format(val_loss))
+
+    model_list.append(net)
+    val_loss_list.append(val_loss)
+
+#determine best model
+lowest_val_loss = 100000
+index = 0
+for i in range(len(model_list)):
+    if val_loss_list[i] <= lowest_val_loss:
+        lowest_val_loss = val_loss_list[i]
+        index = i
+
+#test the best model to get statistics
+net = model_list[i]
+
+#optmizer
+optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
+
+#create new hidden state variables
+test_hidden = net.init_hidden_state(batch_size, train_on_gpu)
+
+#put into eval mode
+net.eval()
+
+test = TensorDataset(torch.FloatTensor(reserved_test_x), torch.FloatTensor(reserved_test_y))
+test_loader = DataLoader(test, batch_size = batch_size, shuffle = True)
+
+test_losses = list()
+
+for inputs, targets in test_loader:
+    
+    inputs = inputs.to(device).long()
+    targets = targets.to(device).long()
+
+    #create new hidden state variables
+    test_hidden = tuple([h.data for h in test_hidden])
+
+    #get output and then calculate loss
+    output, test_hidden = net(inputs, test_hidden)
+    test_loss = criterion(output, targets)
+    test_losses.append(test_loss)
+    #MORE STATS HERE
+
+test_loss=np.mean(test_losses)
+print("Test Loss: {:.6f}".format(test_loss)
